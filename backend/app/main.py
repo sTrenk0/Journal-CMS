ba5ae93ca899c1edd8a13feb1c98905948096bcc
sTrenk0 <Artem3930@proton.me>
2024-11-03 22:44:57 +0100

    added: cors policy

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .database.initial import create_initial_superuser
 from .settings import config
 from .api.core import router
@@ -10,9 +11,21 @@ async def lifespan(*args, **kwargs):
     await create_initial_superuser()
     yield
 
+# put your localhost with your port of frontend app here
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
 fastapi_app = FastAPI(
     lifespan=lifespan,
     version=f"{config.api_version}",
+)
+fastapi_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 fastapi_app.include_router(router)
 
