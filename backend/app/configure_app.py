1a80b2a8505896844ada42361b6b62fc64c5219f
sTrenk0 <Artem3930@proton.me>
2024-11-08 01:52:41 +0100

    added: configure_app file with the create_configured_app function that creates the application and configures it. deleted: logic of creating an application in main.py file

---
+++ backend/app/configure_app.py
@@ -0,0 +1,28 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.api.core import router
+from app.database.initial import create_initial_superuser
+from app.settings import config
+
+
+def create_configured_app() -> FastAPI:
+    @asynccontextmanager
+    async def lifespan(*args, **kwargs):
+        await create_initial_superuser()
+        yield
+
+    app = FastAPI(lifespan=lifespan, version=f"{config.api_version}")
+    # put your localhost with your port of frontend app here
+    origins = ["http://localhost", "http://localhost:8080", "http://localhost:5173/"]
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    app.include_router(router)
+    return app
