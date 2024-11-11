0064473b44324eebd1421875198e937d7b44e34e
sTrenk0 <Artem3930@proton.me>
2024-11-12 00:17:08 +0100

    deleted: test endpoint raise_err

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,15 @@
 from .configure_app import create_configured_app
 
-
 fastapi_app = create_configured_app()
-
-
-@fastapi_app.get("/one")
-def raise_err():
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND, detail=ProductError.NOT_FOUND
-    )
 
 
 # use this in case when you want run app directly without docker
 # if __name__ == "__main__":
-    # import uvicorn
-    # from .settings import config
-    # uvicorn.run("app.main:fastapi_app", host="0.0.0.0", port=config.app_port, reload=True)
+#     import uvicorn
+#     from .settings import config
+#     uvicorn.run(
+#         "app.main:fastapi_app",
+#         host="0.0.0.0",  # noqa: S104 (this interface is only available on the closed docker network)
+#         port=config.app_port,
+#         reload=True,
+#     )
